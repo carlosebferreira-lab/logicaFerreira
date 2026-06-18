b0c9629cfc5913806e15326ba5ae29280462b3a4
carlosebferreira-lab <carlos_eb_ferreira@estudante.sesisenai.org.br>
2026-06-18 17:25:12 -0300

    Progamas de hoje

---
+++ script.js
@@ -0,0 +1,34 @@
+   //ler estagiarios
+   //ler clt 
+   //ler pj 
+
+   //calcular total
+
+   //mostrar resultado    
+  
+  
+  function banheiro(){
+    alert("ja fez ate o exercicio 3?")
+  }
+
+  function pePequeno(){
+    let qtdpares = Number(prompt("quantos pares"))
+    let precoPar = Number(prompt("Preço de cada par?"))
+
+    let total = qtdpares * precoPar
+
+    alert("valor total R$ + total")
+  }
+
+  function peba(){ 
+   // ler o numero de vitorias 
+   let  vitorias = Number(prompt("quantas vitorias?"))
+   // ler o numero de empates
+   let empates = Number(prompt("quantos empates"))
+   // calcular pontos
+   let pontos = vitorias*3 + empates
+   // mostrar resultados
+   alert("o time teu tem " + pontos)
+}
+
+ 
